13f633ffeaf9905155c10675700632ece9693d47
許君愷 / xu jun kai <kevin16021777@gmail.com>
2024-06-24 12:07:18 +0800

    feat: tabBar set

--- NTPUOne/ContentView.swift
+++ NTPUOne/ContentView.swift
@@ -13,6 +13,40 @@ struct ContentView: View {
     @ObservedObject var webManager = WebManager()
     
     var body: some View {
+        TabView{
+            linkView.tabItem {
+                Image(systemName: "house")
+                Text("Home")
+            }
+            trafficView.tabItem {
+                Image(systemName: "car")
+                Text("traffic")
+            }
+            dietView.tabItem{
+                Image(systemName: "fork.knife")
+                Text("food")
+            }
+            timetableView.tabItem{
+                Image(systemName: "list.clipboard")
+                Text("timetable")
+            }
+            aboutView.tabItem{
+                Image(systemName: "info.circle")
+                Text("about")
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+//MARK: - sub page
+
+private extension ContentView{
+    var linkView: some View{
         NavigationView{
             List(webManager.webDatas, rowContent: { web in
                 NavigationLink(destination: WebDetailView(url: web.url)) {
@@ -28,8 +62,25 @@ struct ContentView: View {
             webManager.createData()
         })
     }
-}
-
-#Preview {
-    ContentView()
+    var trafficView: some View{
+        NavigationView(content: {
+            NavigationLink(destination: Text("Destination")) { Text("Navigate") }
+        })
+    }
+    var dietView: some View{
+        NavigationView(content: {
+            NavigationLink(destination: Text("Destination")) { Text("Navigate") }
+        })
+    }
+    var timetableView: some View{
+        NavigationView(content: {
+            NavigationLink(destination: Text("Destination")) { Text("Navigate") }
+        })
+    }
+    var aboutView: some View{
+        NavigationView(content: {
+            NavigationLink(destination: Text("Destination")) { Text("Navigate") }
+        })
+    }
+    
 }
